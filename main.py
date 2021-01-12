90ac9e2d3644d85985cbd0bea8f3211289c8c130
Marc <marcgruita@gmail.com>
2021-01-12 10:04:46 +0200

    more suggestive name for console ui

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from src.ui.gui import GUI
 from src.ui.console_ui import ConsoleUI
 
 response = ""
-while response not in ["UI", "GUI"]:
-    print("What type of ui do you want? (UI/GUI): ", end="")
+while response not in ["CONSOLE", "GUI"]:
+    print("What type of ui do you want? (CONSOLE/GUI): ", end="")
     response = input()
 
 ui = None
-if response == "UI":
+if response == "CONSOLE":
     ui = ConsoleUI()
 elif response == "GUI":
     ui = GUI()
